952c26458aa5cc69b6c35eef6671042306483e60
KarineAlsa <saremana@hotmail.com>
2024-03-17 22:17:46 -0600

    migration added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv'
 import path from 'path';
-
+import { exec } from 'child_process';
 dotenv.config()
 const server = express();
 const server_port =process.env.PORT;
@@ -10,4 +10,16 @@ server.use(express.json());
 
 server.listen(process.env.PORT, () => {
     console.log(`Server listening on http://localhost:${server_port}/`);
+});
+
+const migrateScript = path.join(__dirname, '..', 'src', 'UserManagement', 'Infrastructure', 'Migrations', 'Migration.ts');
+
+const migrationProcess = exec(`ts-node ${migrateScript}`);
+
+migrationProcess.on('exit', (code: number) => {
+    if (code === 0) {
+        console.log('Migración completada con éxito');
+    } else {
+        console.error('Error durante la migración');
+    }
 });
