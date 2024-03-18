90d4b3a13ba1247165070cd4505f482d197ea820
KarineAlsa <saremana@hotmail.com>
2024-03-17 20:06:02 -0600

    initializingRespository

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from 'dotenv'
+import path from 'path';
+
+dotenv.config()
+const server = express();
+const server_port =process.env.PORT;
+server.use(express.json());
+
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server listening on http://localhost:${server_port}/`);
+});
