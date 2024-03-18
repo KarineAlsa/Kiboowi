3205fbb7d789f4be332e0122e67841aa908e074e
KarineAlsa <saremana@hotmail.com>
2024-03-18 00:11:12 -0600

    feature:login

--- src/UserManagement/Infrastructure/Route/UserRoute.ts
+++ src/UserManagement/Infrastructure/Route/UserRoute.ts
@@ -1,11 +1,11 @@
 import  express  from "express";
 import { registerController } from "../Dependencies";
-
+import { loginController } from "../Dependencies";
 
 const userRouter = express.Router();
 
 
 userRouter.post("/register",registerController.run.bind(registerController));
-
+userRouter.post("/login",loginController.run.bind(loginController));
 
 export default userRouter;
