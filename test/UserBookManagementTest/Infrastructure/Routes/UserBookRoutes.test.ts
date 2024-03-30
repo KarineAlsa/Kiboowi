2b3ebb0a0a5e3c6fed69cc352257d9b05b3a2090
KarineAlsa <saremana@hotmail.com>
2024-03-30 13:23:48 -0600

    refactor(UBMT):comillas

--- test/UserBookManagementTest/Infrastructure/Routes/UserBookRoutes.test.ts
+++ test/UserBookManagementTest/Infrastructure/Routes/UserBookRoutes.test.ts
@@ -4,11 +4,11 @@ import app from '../../../../src/app';
 
 describe('UserBookRouter', () => {
   it('responds with status 200 and a list of reading books', async () => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IkthcmluZSBBbGNhemFyIFNhcm1pZW50byIsInVzZXJuYW1lIjoia2FyaWFsc2EiLCJlbWFpbCI6IjIxMzM0MEBkcy51cGNoaWFwYXMuZWR1Lm14IiwiYmlydGhkYXkiOiIyMDAyLTExLTI5IiwiY3JlYXRlRGF0ZSI6IjIwMjQtMDMtMTgiLCJpYXQiOjE3MTE3NjI4MzIsImV4cCI6MTcxMTc2NjQzMn0.zYpLHYjoWGenZgVzyCvCQfuSls0Cw6Wg1YoF_xm5OoI';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IkthcmluZSBBbGNhemFyIFNhcm1pZW50byIsInVzZXJuYW1lIjoia2FyaWFsc2EiLCJlbWFpbCI6IjIxMzM0MEBkcy51cGNoaWFwYXMuZWR1Lm14IiwiYmlydGhkYXkiOiIyMDAyLTExLTI5IiwiY3JlYXRlRGF0ZSI6IjIwMjQtMDMtMTgiLCJpYXQiOjE3MTE3ODExODEsImV4cCI6MTcxMTc4NDc4MX0.imQ-DbQnolpqNJWXZJVPXdcO5RMkw8pB-sDsLMbcthw';
     
     const response = await request(app)
       .get('/user-book/reading-books')
-      .set('Authorization', Bearer ${token});
+      .set('Authorization', `Bearer ${token}`);
 
     expect(response.status).toBe(200);
     expect(response.body.success).toBe(true);
@@ -19,13 +19,13 @@ describe('UserBookRouter', () => {
   });
 
   it('responds with status 200 and success message after adding a book', async () => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IkthcmluZSBBbGNhemFyIFNhcm1pZW50byIsInVzZXJuYW1lIjoia2FyaWFsc2EiLCJlbWFpbCI6IjIxMzM0MEBkcy51cGNoaWFwYXMuZWR1Lm14IiwiYmlydGhkYXkiOiIyMDAyLTExLTI5IiwiY3JlYXRlRGF0ZSI6IjIwMjQtMDMtMTgiLCJpYXQiOjE3MTE3NjI4MzIsImV4cCI6MTcxMTc2NjQzMn0.zYpLHYjoWGenZgVzyCvCQfuSls0Cw6Wg1YoF_xm5OoI';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IkthcmluZSBBbGNhemFyIFNhcm1pZW50byIsInVzZXJuYW1lIjoia2FyaWFsc2EiLCJlbWFpbCI6IjIxMzM0MEBkcy51cGNoaWFwYXMuZWR1Lm14IiwiYmlydGhkYXkiOiIyMDAyLTExLTI5IiwiY3JlYXRlRGF0ZSI6IjIwMjQtMDMtMTgiLCJpYXQiOjE3MTE3ODExODEsImV4cCI6MTcxMTc4NDc4MX0.imQ-DbQnolpqNJWXZJVPXdcO5RMkw8pB-sDsLMbcthw';
 
     const response = await request(app)
       .post('/user-book/add-book')
-      .set('Authorization', Bearer ${token})
+      .set('Authorization', `Bearer ${token}`)
       .send({
-        idBook: 2002,
+        idBook: 2006,
         state: 2,
         bookName: 'nuevo libro',
         authorName: 'nuevo autor',
@@ -36,15 +36,15 @@ describe('UserBookRouter', () => {
     expect(response.body.success).toBe(true);
     expect(response.body.message).toBe('Libro añadido');
     expect(response.body.data).toBeDefined();
-    expect(response.body.data.idBook).toBe(2002); 
+    expect(response.body.data.idBook).toBe(2006); 
   });  
 
   it('responds with status 200 and a list of to read books', async () => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IkthcmluZSBBbGNhemFyIFNhcm1pZW50byIsInVzZXJuYW1lIjoia2FyaWFsc2EiLCJlbWFpbCI6IjIxMzM0MEBkcy51cGNoaWFwYXMuZWR1Lm14IiwiYmlydGhkYXkiOiIyMDAyLTExLTI5IiwiY3JlYXRlRGF0ZSI6IjIwMjQtMDMtMTgiLCJpYXQiOjE3MTE3NjI4MzIsImV4cCI6MTcxMTc2NjQzMn0.zYpLHYjoWGenZgVzyCvCQfuSls0Cw6Wg1YoF_xm5OoI';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IkthcmluZSBBbGNhemFyIFNhcm1pZW50byIsInVzZXJuYW1lIjoia2FyaWFsc2EiLCJlbWFpbCI6IjIxMzM0MEBkcy51cGNoaWFwYXMuZWR1Lm14IiwiYmlydGhkYXkiOiIyMDAyLTExLTI5IiwiY3JlYXRlRGF0ZSI6IjIwMjQtMDMtMTgiLCJpYXQiOjE3MTE3ODExODEsImV4cCI6MTcxMTc4NDc4MX0.imQ-DbQnolpqNJWXZJVPXdcO5RMkw8pB-sDsLMbcthw';
     
     const response = await request(app)
       .get('/user-book/to-read-books')
-      .set('Authorization', Bearer ${token});
+      .set('Authorization', `Bearer ${token}`);
 
     expect(response.status).toBe(200);
     expect(response.body.success).toBe(true);
@@ -55,11 +55,11 @@ describe('UserBookRouter', () => {
   });
 
   it('responds with status 200 and a list of read books', async () => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IkthcmluZSBBbGNhemFyIFNhcm1pZW50byIsInVzZXJuYW1lIjoia2FyaWFsc2EiLCJlbWFpbCI6IjIxMzM0MEBkcy51cGNoaWFwYXMuZWR1Lm14IiwiYmlydGhkYXkiOiIyMDAyLTExLTI5IiwiY3JlYXRlRGF0ZSI6IjIwMjQtMDMtMTgiLCJpYXQiOjE3MTE3NjI4MzIsImV4cCI6MTcxMTc2NjQzMn0.zYpLHYjoWGenZgVzyCvCQfuSls0Cw6Wg1YoF_xm5OoI';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IkthcmluZSBBbGNhemFyIFNhcm1pZW50byIsInVzZXJuYW1lIjoia2FyaWFsc2EiLCJlbWFpbCI6IjIxMzM0MEBkcy51cGNoaWFwYXMuZWR1Lm14IiwiYmlydGhkYXkiOiIyMDAyLTExLTI5IiwiY3JlYXRlRGF0ZSI6IjIwMjQtMDMtMTgiLCJpYXQiOjE3MTE3ODExODEsImV4cCI6MTcxMTc4NDc4MX0.imQ-DbQnolpqNJWXZJVPXdcO5RMkw8pB-sDsLMbcthw';
     
     const response = await request(app)
       .get('/user-book/read-books')
-      .set('Authorization', Bearer ${token});
+      .set('Authorization', `Bearer ${token}`);
 
     expect(response.status).toBe(200);
     expect(response.body.success).toBe(true);
