e0da1b78feefbab0167fe74237e40d7fa380189d
AylinMalpica09 <100156363+AylinMalpica09@users.noreply.github.com>
2024-03-31 13:07:03 -0600

    src update-user-book

--- src/UserBookManagement/Infrastructure/Routes/UserBookRoutes.ts
+++ src/UserBookManagement/Infrastructure/Routes/UserBookRoutes.ts
@@ -1,5 +1,5 @@
 import { VerifyToken } from "../Controller/Middleware/VerifyToken";
-import { addUserBookController, getReadingBooksController, getToReadBooksController, getReadBooksController } from "../Dependencies";
+import { addUserBookController, getReadingBooksController, getToReadBooksController, getReadBooksController, updateUserBookController } from "../Dependencies";
 import  express  from "express";
 const userBookRouter = express.Router();
 
@@ -7,5 +7,6 @@ userBookRouter.post("/add-book/",VerifyToken,addUserBookController.run.bind(addU
 userBookRouter.get("/reading-books/",VerifyToken,getReadingBooksController.run.bind(getReadingBooksController));
 userBookRouter.get("/to-read-books/",VerifyToken,getToReadBooksController.run.bind(getToReadBooksController));
 userBookRouter.get("/read-books/",VerifyToken,getReadBooksController.run.bind(getReadBooksController));
+userBookRouter.put("/update-book/:idUserBook",VerifyToken,updateUserBookController.run.bind(updateUserBookController));
 
 export default userBookRouter;
